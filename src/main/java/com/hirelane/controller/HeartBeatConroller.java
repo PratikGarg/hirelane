22b0eb6692360e1f2f925c274bb5d1ce6f77a093
Pratik Garg <saytopratik@gmail.com>
2015-01-06 12:26:15 +0530

    Created spring mvc skeleton

---
+++ src/main/java/com/hirelane/controller/HeartBeatConroller.java
@@ -0,0 +1,17 @@
+package com.hirelane.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HeartBeatConroller {
+
+	@RequestMapping("/")
+	public String printHelloWorld(Model model) {
+		model.addAttribute("message", "Alive!");
+
+		return "hearbeat";
+	}
+}
